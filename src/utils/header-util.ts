ab39f21115923c6d9aa9c5d49e1691c6af1e430d
whilconn <liuweikang@xiao100.com>
2022-08-17 10:30:27 +0800

    refactor: 完善 fixed header 解析逻辑

--- src/utils/header-util.ts
+++ src/utils/header-util.ts
@@ -26,20 +26,38 @@ function locateHeaderBottom() {
 
 function detectHeader(y: number) {
   const nodes = document.elementsFromPoint(window.innerWidth / 2, y) as HTMLElement[];
-  return nodes.reverse().find((node) => isFixedHeader(node));
+
+  for (let node, i = nodes.length - 1; i > -1; i--) {
+    node = nodes[i];
+
+    // 退出：样式名包含mask、modal
+    const isMask = /mask|modal|dialog/i.test(node.className);
+    if (isMask) break;
+
+    // 退出：全屏浮窗
+    const isFullScreen = node.scrollWidth >= window.innerWidth && node.scrollHeight >= window.innerHeight;
+    const style = getComputedStyle(node);
+    const isFixed = FIXED_POSITIONS.includes(style.position);
+    if (isFullScreen && isFixed) break;
+
+    if (isFixedHeader(node)) return node;
+  }
+
+  return null;
 }
 
 function isFixedHeader(node: HTMLElement) {
   if (!node) return false;
 
-  const selector = 'header,.header';
-  const style = getComputedStyle(node);
+  // header选择器容易误判，因此不作为判定条件
+  // const hasHeaderSelector = /head(er)?/i.test(`${node.tagName} ${node.className}`);
 
+  const style = getComputedStyle(node);
   const isFixed = FIXED_POSITIONS.includes(style.position);
   const isUpper = +style.zIndex > 10;
 
   const ltHalfHeight = node.scrollHeight < Math.max(window.innerHeight / 2, 200);
   const gtQuarterWidth = node.scrollWidth > Math.min(window.innerWidth / 4, 2000);
 
-  return (node.matches(selector) || isFixed || isUpper) && ltHalfHeight && gtQuarterWidth;
+  return (isFixed || isUpper) && ltHalfHeight && gtQuarterWidth;
 }
