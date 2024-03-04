c4484e038939ea4fffd8ed28d8174b1492a1bfe3
whilconn <798490319@qq.com>
2024-03-04 19:32:13 +0800

    chore: 优化解析规则更新逻辑

--- src/shared/resolve-rules.ts
+++ src/shared/resolve-rules.ts
@@ -1,6 +1,6 @@
 import * as micromatch from 'micromatch';
 import resolveRuleJson from './resolve-rules.json';
-import { loadSettings, saveSettings, Settings } from '../extension-utils/settings';
+import { saveSettings } from '../extension-utils/settings';
 
 export const RESOLVE_RULES_VERSION = resolveRuleJson.version as string;
 export const RESOLVE_RULES: ResolveRule[] = resolveRuleJson.rules;
@@ -14,23 +14,17 @@ export function matchResolveRule(rules: ResolveRule[]) {
 
 /** 更新解析规则 **/
 export async function updateResolveRules() {
-  const settings: Settings = await loadSettings();
-
   const resolveConfigUrl = import.meta.env.VITE_RESOLVE_URL as string;
   const resolveConfigJson = (await fetch(resolveConfigUrl).then((r) => {
     return r.ok ? r.json() : new Error(`${r.status} ${r.statusText}`);
   })) as { version: string; rules: ResolveRule[] };
 
-  if (!resolveConfigJson?.version) return Promise.reject(resolveConfigJson);
+  if (!resolveConfigJson?.version || !resolveConfigJson?.rules) return Promise.reject(resolveConfigJson);
 
-  if (!settings?.resolveRulesVersion || resolveConfigJson.version > settings.resolveRulesVersion) {
-    return saveSettings({
-      resolveRules: resolveConfigJson.rules,
-      resolveRulesVersion: resolveConfigJson.version,
-    });
-  }
-
-  return Promise.reject(new Error('更新失败'));
+  return saveSettings({
+    resolveRules: resolveConfigJson.rules,
+    resolveRulesVersion: resolveConfigJson.version,
+  });
 }
 
 export type ResolveRule = {
